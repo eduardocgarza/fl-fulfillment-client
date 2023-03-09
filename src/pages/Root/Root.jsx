65d2feaba2274a931028e7caebfdb86c4859229a
Eduardo Garza <eduardo@garza.ca>
2023-03-09 02:40:14 -0800

    Added initial pages.

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,10 +1,10 @@
-import MainNav from 'components/MainNav/MainNav'
-import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import MainNav from "components/MainNav/MainNav"
+import React from "react"
+import { Outlet } from "react-router-dom"
 
 export default function Root() {
   return (
-    <div>
+    <div className="py-2 bg-gray-100">
       <MainNav />
       <Outlet />
     </div>
